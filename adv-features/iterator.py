776c29712ab46a29645087d0deec211817110696
perrywzp <wangzhipei1121@163.com>
2018-03-02 23:48:37 +0800

    整理函数式编程语法

--- adv-features/iterator.py
+++ adv-features/iterator.py
@@ -1,3 +1,14 @@
 # -*- coding: utf-8 -*-
 from collections import Iterable
 isinstance([], Iterable)
+
+for x in [1, 2, 3, 4, 5]:
+    pass
+
+
+it = iter([1, 2, 3, 4, 5])
+while True:
+    try:
+        x = next(it)
+    except StopIteration:
+        break
